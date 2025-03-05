5acac1a02b00ad313677240a3050ca6539ba9537
devopus-git <ushaleadcherukuri@gmail.com>
2025-03-05 22:13:25 +0530

    array

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=("APPLE" "KIWI" "ORANGE")
+
+echo "First Fruit name is:${Fruits[0]}"
+echo "Second Fruit name is:${Fruits[1]}"
+echo "Third Fruit name is:${Fruits[2]}"
+echo "All Fruits names are:${Fruits[@]}"
