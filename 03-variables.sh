2aee2ccae0257ba847328f255eb84083ca0db75e
devopus-git <ushaleadcherukuri@gmail.com>
2025-03-05 21:18:09 +0530

    Modified the mistake in above script

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 Person1=Usha
 Person2=Rani
 
-echo "Person1=Hai Person2, how are you?"
-echo "Person2=Hey Person1, I am doing fine. What about you?"
-echo "Person1=How are your kids?"
-echo "Person2=They are quite busy with their studies."
+echo "$Person1=Hai $Person2, how are you?"
+echo "$Person2=Hey $Person1, I am doing fine. What about you?"
+echo "$Person1=How are your kids?"
+echo "$Person2=They are quite busy with their studies."
