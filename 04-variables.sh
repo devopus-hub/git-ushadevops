8bafc3cc4d69c819480c055afc64a05516cb064a
devopus-git <ushaleadcherukuri@gmail.com>
2025-03-05 21:31:56 +0530

    arguments

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,5 +5,5 @@ Person2=$2
 
 echo "$Person1=Hai $Person2, how are you?"
 echo "$Person2=Hey $Person1, I am doing fine. What about you?"
-echo "$Person1=How are your kids?"
+echo "$Person1=I am also fine,how are your kids?"
 echo "$Person2=They are quite busy with their studies."
