c66197e2ba0ba361e9c92338bd87d933d391f472
devopus-git <ushaleadcherukuri@gmail.com>
2025-03-05 21:28:57 +0530

    arguments

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "Usha=Hai Rani, how are you?"
+echo "Rani=Hey Usha, I am doing fine. What about you?"
+echo "Usha=How are your kids?"
+echo "Rani=They are quite busy with their studies."
