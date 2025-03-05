572b4b162ace4f026e1c0d10af23ec0dfcb2775d
devopus-git <ushaleadcherukuri@gmail.com>
2025-03-05 20:35:42 +0530

    I have created shell script with some conversation between Usha and Rani

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Usha=Hai Rani, how are you?"
+echo "Rani=Hey Usha, I am doing fine. What about you?"
+echo "Usha=How are your kids?"
+echo "Rani=They are quite busy with their studies."
