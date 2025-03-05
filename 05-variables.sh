770697e7a5ae55eda356f443aeb1c518a9b27fe2
devopus-git <ushaleadcherukuri@gmail.com>
2025-03-05 21:54:34 +0530

    arguments

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Please enter your username:read -s username"
+echo "Please enter your username:$username""
+read -s username
 
-echo "Username entered is:$username"
 echo "Please enter your password:"
 read -s password
