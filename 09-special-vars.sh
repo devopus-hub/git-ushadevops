0369eb8326e6ab1bf8d361171b24d15dc2a26499
devopus-git <ushaleadcherukuri@gmail.com>
2025-03-26 09:50:37 +0530

    variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -7,5 +7,5 @@ echo "current working direcoty: $PWD"
 echo "Home directory of current user: $HOME"
 echo "PID of the script executing now: $$"
 
-sleep 20 &
+sleep 100 &
 echo "PID of last background command:$!"
