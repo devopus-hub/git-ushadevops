b5ad62a79b37c126dbb6739d6be62decbec51a31
devopus-git <ushaleadcherukuri@gmail.com>
2025-03-26 10:05:24 +0530

    variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -8,3 +8,7 @@ echo "Home directory of current user: $HOME"
 echo "PID of the script executing now: $$"
 sleep 30 &
 echo "PID of last background command: $!"
+
+#we may or may not give space before variable value like $@, $# etc..
+#If we want to run sleep command don't put & symbol (sleep 30)
+#If we put & symbol it won't sleep and run next command
