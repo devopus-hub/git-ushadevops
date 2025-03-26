e048bae05e6b5267b6fdbc829ebfa7f07b9c4614
devopus-git <ushaleadcherukuri@gmail.com>
2025-03-26 09:34:30 +0530

    variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -3,7 +3,7 @@
 echo "Total variables passed to the script:$@"
 echo "No.of variables passed:$#"
 echo "Script name:$0"
-echo "current working direcoty:$pwd"
+echo "current working direcoty:$PWD"
 echo "Home directory of current user:$HOME"
 echo "PID of the script executing now:$$"
 
