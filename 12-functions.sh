e1d508fbb18c9c14f866132c6b7eef9814e24089
devopus-git <ushaleadcherukuri@gmail.com>
2025-03-27 20:56:48 +0530

    conditions

--- 12-functions.sh
+++ 12-functions.sh
@@ -3,7 +3,7 @@ $!/bin/bash
 USERID=$(id -u)
 #echo "user id is: $USERID"
 
-CHECK_ROOT(){                                           #its a function to check root user
+CHECK_ROOT(){                                           
     if [ $? -ne 0 ]
     then
         echo "Please run this script with root privilages"
