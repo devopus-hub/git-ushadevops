36fab94245e69f48032059799a7994140cae7783
devopus-git <ushaleadcherukuri@gmail.com>
2025-04-01 21:52:35 +0530

    Functions

--- 12-functions-1.sh
+++ 12-functions-1.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
-if [ $USERID -ne 0 ]
-then 
-    echo "Please run this script with root privilages"
-    exit 1
-fi
+USERID=$(id -u)
+CHECK_ROOT () {
+    if [ $USERID -ne 0 ]
+    then 
+        echo "Please run this script with root privilages"
+        exit 1
+    fi
+}
 
+VALIDATE () {
+    if [ $1 -ne 0 ]
+    then
+        echo "$2 installing is .... Failed"
+        exit 1
+    else
+        echo "$2 installing is ..... Success"
+    fi
+}
+
+
+CHECK_ROOT
 dnf list installed git
 
 if [ $? -ne 0 ]
 then
     echo "Git is not installed, going to install now..."
     dnf install git -y
-    if [ $? -ne 0 ]
-    then
-        echo "Git installation is not success....check it..."
-        exit 1
-    else
-        echo "Git installed successfully"
-    fi
+    VALIDATE $? "installing Git"
 else
     echo "Git installed already, nothing to do..."
 fi
@@ -29,12 +38,7 @@ if [ $? -ne 0 ]
 then
     echo "Mysql is not installed, going to install now..."
     dnf install mysql -y
-    if [ $? -ne 0 ]
-    then
-        echo "Mysql installation is not successful....check it"
-        exit 1
-    else
-        echo "Mysql installation is successful"
-    fi
-else    echo "Mysql is installed successfully..nothing to do.."
+    VALIDATE $? "installing MYSQL"
+else
+    echo "Mysql is installed successfully..nothing to do.."
 fi
